51791e8582d46e64d935d6ddca9dc7106d5ccfae
mmekhanov <mikhail.mekhanov@eurochem.ru>
2017-06-21 18:47:23 +0300

    Fix profile env rights

--- rbenv-install-system-wide.sh
+++ rbenv-install-system-wide.sh
@@ -37,7 +37,8 @@ echo '  export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
 echo '  eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
 echo 'fi'  >> /etc/profile.d/rbenv.sh
 
-chmod +x /etc/profile.d/rbenv.sh
+# depends on Centos version
+# chmod +x /etc/profile.d/rbenv.sh
 
 # Install ruby-build:
 pushd /tmp
